9cfd22a18fbcbb98d46f4120de131871f51d6e93
Alexandra <149502066+iBukkoG104@users.noreply.github.com>
2025-11-25 08:27:13 +0100

    Update lwipopts.h
    added a few configuration lines

--- PPP-tests/lwipopts.h
+++ PPP-tests/lwipopts.h
@@ -4,7 +4,7 @@
 // No OS
 #define NO_SYS                          1
 #define SYS_LIGHTWEIGHT_PROT            0
-#define PPP_INPROC_IRQ_SAFE				1
+// #define PPP_INPROC_IRQ_SAFE				1
 
 // Memory
 #define MEM_ALIGNMENT                   4
@@ -25,6 +25,11 @@
 #define PPP_SUPPORT                     1
 #define PPPOS_SUPPORT                   1
 #define PAP_SUPPORT                     1
-#define CHAP_SUPPORT                    1
+#define CHAP_SUPPORT                    0
+
+
+// GEMINI(TM) ADDITIONS
+#define LWIP_SOCKET			0
+#define LWIP_NETCONN		0
 
 #endif
